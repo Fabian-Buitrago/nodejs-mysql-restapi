406eef3ceb33fe5209d80ff2ea8c3223c6f9611f
Fabian Buitrago <fabianbuitragov@gmail.com>
2023-01-31 17:08:57 -0500

    Delete db.js

--- src/db.js
+++
@@ -1,9 +0,0 @@
-import { createPool } from "mysql2/promise";
-
-export const pool = createPool({
-  host: "localhost",
-  user: "root",
-  password: "admin",
-  port: 3306,
-  database: "companydb",
-});
